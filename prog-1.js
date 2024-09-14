d75d42d40c5d3824e1877c42a6745ca24d9e3741
navyamocharla <navya20042@gmail.com>
2024-09-14 11:14:05 +0530

    adding my file

--- prog-1.js
+++ prog-1.js
@@ -1,1 +1,17 @@
-console.log('hellow world');
+function reverseWordsInString(str) {
+    // Split the string into an array of words
+    let wordsArray = str.split(" ");
+
+    // Reverse each word in the array
+    let reversedWordsArray = wordsArray.map(word => word.split("").reverse().join(""));
+
+    // Join the reversed words back into a string
+    let reversedString = reversedWordsArray.join(" ");
+
+    return reversedString;
+}
+
+// Example usage:
+let inputString = "Hello World";
+let result = reverseWordsInString(inputString);
+console.log(result);  
